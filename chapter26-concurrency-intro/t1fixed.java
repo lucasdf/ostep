293af79ff0ea21805d129f87b84f51bc96e3420d
Lucas Ferreira <github@lucasferreira.org>
2020-04-11 19:08:12 -0300

    Add Java version and Java fixed race condition

---
+++ chapter26-concurrency-intro/t1fixed.java
@@ -0,0 +1,38 @@
+class T1Fixed {
+
+  static class Counter {
+    private int count = 0;
+    public synchronized void increment() {
+      this.count++;
+    }
+  }
+
+  static class MyThread implements Runnable {
+    String identifier;
+    public MyThread(String identifier) {
+      this.identifier = identifier;
+    }
+    public void run() {
+      for (int i = 0; i < 1000000; i++) {
+        counter.increment();
+      }
+      System.out.format("%s: done\n", identifier);
+    }
+  }
+
+  static Counter counter = new Counter();
+  public static void main(String args[]) {
+    System.out.format("main: begin (counter = %d)\n", counter.count);
+    Thread thread1 = new Thread(new MyThread("A"));
+    Thread thread2 = new Thread (new MyThread("B"));
+    thread1.start();
+    thread2.start();
+    try{
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException ex) {
+      System.exit(1);
+    }
+    System.out.format("main: done with both (counter = %d)\n", counter.count);
+  }
+}
